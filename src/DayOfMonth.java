9b88b82a8f7f0ecaee1f1fcaf29de04d806f89bd
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-08-04 13:45:19 +0700

    bai6

--- src/DayOfMonth.java
+++ src/DayOfMonth.java
@@ -6,7 +6,7 @@ public class DayOfMonth {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Which month that you want to count days? ");
+        System.out.println("Which month that you want to count days? ");
         int month = scanner.nextInt();
         String daysInMonth;
 
